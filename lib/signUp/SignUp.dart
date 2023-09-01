a7fdb940784661a93897e0df2c23a6aba0f6c47d
Abdo Adel <bidoadel@gmail.com>
2023-09-02 02:46:50 +0300

    bgColor.blue&hello

--- lib/signUp/SignUp.dart
+++ lib/signUp/SignUp.dart
@@ -12,7 +12,7 @@ class _SignupState extends State<Signup> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
-      body: Text("hi"),
+      body: Text("hi hi "),
       appBar: AppBar(),
     );
   }
